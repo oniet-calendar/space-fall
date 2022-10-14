040235ef3d23954a9c9ec2017d7b4df7fd279a53
JuanSaz <juan.snchez03@gmail.com>
2022-10-13 21:54:57 -0300

    feature: agregar fondo

--- main.py
+++ main.py
@@ -4,21 +4,29 @@ import sys, pygame
 pygame.init()
 
 # tamaño de ventana
-size = 800, 600
+size = 600, 900
 screen = pygame.display.set_mode(size)
 
 # titulo de la ventana
 pygame.display.set_caption("Space Jump")
 
+#cargar imagenes
+bg_image = pygame.image.load("bg.png").convert_alpha()
+
 # comienzo del juego
 running = True
 while running:
+
+  #imprimir fondo
+  screen.blit(bg_image, (0,0))
+
   # capturador de eventos
   for event in pygame.event.get():
-    
     # detección de salida de ventana
     if event.type == pygame.QUIT:
       run = False
+      # salir de pygame
+      pygame.quit()
 
-# salir de pygame
-pygame.quit()
+  pygame.display.update()
+
