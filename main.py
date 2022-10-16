9ea6b2cbeb109face70f2fec582ed39b65d96902
Federico Torres <federicofederico881@gmail.com>
2022-10-15 22:15:41 -0300

    refactor: aplicar formater

--- main.py
+++ main.py
@@ -9,39 +9,65 @@ size = 600, 900
 screen = pygame.display.set_mode(size)
 
 # Font
-font = pygame.font.Font('freesansbold.ttf',28)
-def getFont(fontSize):
-  return pygame.font.Font('freesansbold.ttf', fontSize)
+font = pygame.font.Font("freesansbold.ttf", 28)
 
-#cargar imagenes 
+
+def getFont(fontSize):
+    return pygame.font.Font("freesansbold.ttf", fontSize)
+
+
+# cargar imagenes
 bg_image = pygame.image.load("img/background.png").convert_alpha()
 pygame.display.set_icon(pygame.image.load("img/logo.png").convert_alpha())
 player_sprite = pygame.image.load("img/Astronaut_Falling.png").convert_alpha()
 
 # Menu principal
-def main_menu(): 
-    # titulo de la ventana 
+def main_menu():
+    # titulo de la ventana
     pygame.display.set_caption("Space Fall")
     while True:
-        screen.blit(bg_image, (0,  0))
-        screen.blit(bg_image, (0,  0))
+        screen.blit(bg_image, (0, 0))
+        screen.blit(bg_image, (0, 0))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
- 
+
         MENU_TEXT = getFont(75).render("Space Fall", True, "#ffffff")
         MENU_RECT = MENU_TEXT.get_rect(center=(250, 140))
- 
-        PLAY_BUTTON = Button(image=pygame.image.load("img/play.png"), pos=(300, 350), 
-                            text_input="Jugar", font=getFont(50), base_color="#d7fcd4", hovering_color="White")
-        TUTORIAL_BUTTON = Button(image=pygame.image.load("img/quit.png"), pos=(300, 480), 
-                            text_input="Tutorial", font=getFont(40), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("img/quit.png"), pos=(300, 610), 
-                            text_input="Salir del juego", font=getFont(30), base_color="#d7fcd4", hovering_color="White")
+
+        PLAY_BUTTON = Button(
+            image=pygame.image.load("img/play.png"),
+            pos=(300, 350),
+            text_input="Jugar",
+            font=getFont(50),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
+        TUTORIAL_BUTTON = Button(
+            image=pygame.image.load("img/quit.png"),
+            pos=(300, 480),
+            text_input="Tutorial",
+            font=getFont(40),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
+        QUIT_BUTTON = Button(
+            image=pygame.image.load("img/quit.png"),
+            pos=(300, 610),
+            text_input="Salir del juego",
+            font=getFont(30),
+            base_color="#d7fcd4",
+            hovering_color="White",
+        )
         screen.blit(MENU_TEXT, MENU_RECT)
-        screen.blit(pygame.transform.flip(pygame.transform.scale(player_sprite, (120,120)),True,False),(430,60))
+        screen.blit(
+            pygame.transform.flip(
+                pygame.transform.scale(player_sprite, (120, 120)), True, False
+            ),
+            (430, 60),
+        )
         for button in [PLAY_BUTTON, TUTORIAL_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(screen)
-        
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
@@ -50,12 +76,12 @@ def main_menu():
                 if PLAY_BUTTON.checkForInput(MENU_MOUSE_POS):
                     play()
                 if TUTORIAL_BUTTON.checkForInput(MENU_MOUSE_POS):
-                    pass 
+                    pass
                 if QUIT_BUTTON.checkForInput(MENU_MOUSE_POS):
                     pygame.quit()
                     sys.exit()
 
         pygame.display.update()
 
+
 main_menu()
- 
--- play.py
+++ play.py
@@ -1,39 +1,47 @@
-import pygame,sys, random
+import pygame, sys, random
 
 BLANCO = (255, 255, 255)
 size = 600, 900
 screen = pygame.display.set_mode(size)
 
-#set FPS
+# set FPS
 clock = pygame.time.Clock()
 FPS = 60
 
-#cargar imagenes
+# cargar imagenes
 bg_image = pygame.image.load("img/background.png").convert_alpha()
 player_sprite = pygame.image.load("img/Astronaut_Falling.png").convert_alpha()
 asteroid_image = pygame.image.load("img/asteroid.png").convert_alpha()
 
-#Variables
-GRAVITY = 1                                                             #Esta variable se encarga de modificar el valor de la gravedad
+# Variables
+GRAVITY = 1  # Esta variable se encarga de modificar el valor de la gravedad
 
-#scrolling bg
+# scrolling bg
 def draw_bg(bg_scroll):
-  screen.blit(bg_image, (0,0 + bg_scroll))                              #Usa dos fondos p/ dar continuidad, cuando llega al tope de ambos, reinicia el bg_scroll y vuelve a empezar desde 0
-  screen.blit(bg_image, (0, -900 + bg_scroll))
+    screen.blit(
+        bg_image, (0, 0 + bg_scroll)
+    )  # Usa dos fondos p/ dar continuidad, cuando llega al tope de ambos, reinicia el bg_scroll y vuelve a empezar desde 0
+    screen.blit(bg_image, (0, -900 + bg_scroll))
+
 
 class Asteroid(object):
     def __init__(self, rank):
         self.rank = 1
         if self.rank == 1:
-          self.image = asteroid_image                              #Inicializa y reescala el sprite
-        self.w = 47                                               #Dependiendo del rango, se puede multiplicar el tamaño para hacer asteroides mas grandes
+            self.image = asteroid_image  # Inicializa y reescala el sprite
+        self.w = 47  # Dependiendo del rango, se puede multiplicar el tamaño para hacer asteroides mas grandes
         self.h = 47
-        self.ranPoint = (random.randrange(5 ,580), -15)                   #Donde spawnean los asteroides, entre un valor random del ancho de la pantalla y posicion 0 arriba
+        self.ranPoint = (
+            random.randrange(5, 580),
+            -15,
+        )  # Donde spawnean los asteroides, entre un valor random del ancho de la pantalla y posicion 0 arriba
         self.x, self.y = self.ranPoint
-        self.xv = 0                                 
-        self.yv = 1 * 5                                                 #Velocidad vertical  
-        self.rect = pygame.Rect(0, 0, self.w, self.h)                   #Agrega un rectangulo para colisiones
-        self.rect.center = (65,15)
+        self.xv = 0
+        self.yv = 1 * 5  # Velocidad vertical
+        self.rect = pygame.Rect(
+            0, 0, self.w, self.h
+        )  # Agrega un rectangulo para colisiones
+        self.rect.center = (65, 15)
 
     def draw(self, screen):
         screen.blit(asteroid_image, (self.x, self.y))
@@ -41,133 +49,157 @@ class Asteroid(object):
         self.rect.x = self.xv + self.x
         self.rect.y += self.yv
 
-class Player():                                                         #Clase del jugador 
-  def __init__(self, x, y):
-    self.image = pygame.transform.scale(player_sprite, (80,80))         #Inicializa y reescala el sprite
-    self.height = 75
-    self.width = 40        
-    self.rect = pygame.Rect(0, 0, self.width, self.height)              #Agrega un rectangulo para colisiones
-    self.rect.center = (x,y)                                            #Posiciona este rectangulo en las coordenadas iniciales
-    self.vel_y = 0
-    self.flip = False                                                   #Inicia con la imagen flipped False
-    self.cool_down_count = 0  
-    jetpack_amount = 10
 
-  def cool_down(self):
-    if self.cool_down_count >= 20:
-      self.cool_down_count == 0
-    elif self.cool_down_count > 0:
-      self.cool_down_count += 1
-  
-  def move(self):                                                       #Dependiendo de que tecla se toca:
-    scroll = 0
-    dx = 0 
-    dy = 0
-    self.cool_down()
+class Player:  # Clase del jugador
+    def __init__(self, x, y):
+        self.image = pygame.transform.scale(
+            player_sprite, (80, 80)
+        )  # Inicializa y reescala el sprite
+        self.height = 75
+        self.width = 40
+        self.rect = pygame.Rect(
+            0, 0, self.width, self.height
+        )  # Agrega un rectangulo para colisiones
+        self.rect.center = (
+            x,
+            y,
+        )  # Posiciona este rectangulo en las coordenadas iniciales
+        self.vel_y = 0
+        self.flip = False  # Inicia con la imagen flipped False
+        self.cool_down_count = 0
+        jetpack_amount = 10
 
-    key = pygame.key.get_pressed()
-    if key[pygame.K_a]:
-      dx -= 12
-      self.flip = True
-    if key[pygame.K_d]:
-      dx += 12
-      self.flip = False
-    
-    for event in pygame.event.get():
-      if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-           if self.cool_down_count == 0:
-             self.vel_y = -20
-             self.cool_down_count = 1
+    def cool_down(self):
+        if self.cool_down_count >= 20:
+            self.cool_down_count == 0
+        elif self.cool_down_count > 0:
+            self.cool_down_count += 1
 
-           if self.cool_down_count == 20:
-             self.vel_y = -20
-             self.cool_down_count = 1
-    
-    #Seteo gravedad
-    self.vel_y += GRAVITY
-    dy += self.vel_y
+    def move(self):  # Dependiendo de que tecla se toca:
+        scroll = 0
+        dx = 0
+        dy = 0
+        self.cool_down()
 
-    #delimitar el movimiento para evitar cruzar los margenes verticales
-    if self.rect.bottom + dy > 900:
-      pygame.quit()
-      sys.exit()
-      #dy = 0      #TEST: para no caer en el vacio, se frena la velocidad de caída
+        key = pygame.key.get_pressed()
+        if key[pygame.K_a]:
+            dx -= 12
+            self.flip = True
+        if key[pygame.K_d]:
+            dx += 12
+            self.flip = False
 
-    #delimitar el movimiento para evitar cruzar los margenes laterales
-    if self.rect.left + dx <0:
-      dx = -self.rect.left
-    if self.rect.right + dx > 600:
-      dx = 600 - self.rect.right
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if self.cool_down_count == 0:
+                        self.vel_y = -20
+                        self.cool_down_count = 1
 
-    if self.rect.top <= 200:
-      if self.vel_y < 0:
-        scroll = -dy
+                    if self.cool_down_count == 20:
+                        self.vel_y = -20
+                        self.cool_down_count = 1
 
-    #actualizo el valor de movimiento por variables almacenando movimiento en pixeles
-    self.rect.x += dx
-    self.rect.y += dy + scroll
+        # Seteo gravedad
+        self.vel_y += GRAVITY
+        dy += self.vel_y
 
-    return scroll
+        # delimitar el movimiento para evitar cruzar los margenes verticales
+        if self.rect.bottom + dy > 900:
+            pygame.quit()
+            sys.exit()
+            # dy = 0      #TEST: para no caer en el vacio, se frena la velocidad de caída
 
-  def draw(self):                                                       #Funcion dedicada a imprimir el sprite // Dependiendo su direccion, se flipea el sprite
-    screen.blit(pygame.transform.flip(self.image, self.flip, False), (self.rect.x - 20, self.rect.y - 5))
-    pygame.draw.rect(screen, BLANCO, self.rect, 2)
+        # delimitar el movimiento para evitar cruzar los margenes laterales
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > 600:
+            dx = 600 - self.rect.right
+
+        if self.rect.top <= 200:
+            if self.vel_y < 0:
+                scroll = -dy
+
+        # actualizo el valor de movimiento por variables almacenando movimiento en pixeles
+        self.rect.x += dx
+        self.rect.y += dy + scroll
+
+        return scroll
+
+    def draw(
+        self,
+    ):  # Funcion dedicada a imprimir el sprite // Dependiendo su direccion, se flipea el sprite
+        screen.blit(
+            pygame.transform.flip(self.image, self.flip, False),
+            (self.rect.x - 20, self.rect.y - 5),
+        )
+        pygame.draw.rect(screen, BLANCO, self.rect, 2)
+
 
 def play():
-  # Font
-  font = pygame.font.Font('freesansbold.ttf',28)
-  score = 0
-  scroll = 0
-  bg_scroll = 0
-  asteroids = []
-  asteroidCount = 0
+    # Font
+    font = pygame.font.Font("freesansbold.ttf", 28)
+    score = 0
+    scroll = 0
+    bg_scroll = 0
+    asteroids = []
+    asteroidCount = 0
 
-  # comienzo del juego
-  running = True
+    # comienzo del juego
+    running = True
 
-  player = Player(300, 400)                                               #Inicializa al Player en X=300 Y=400
-  
-  
-  
-  while running:
+    player = Player(300, 400)  # Inicializa al Player en X=300 Y=400
 
-    clock.tick(FPS)                                                       #Setea los FPS a 60
-    asteroidCount += 1
-    scroll = player.move()                                                #Agrega funcionalidad de movimiento en la clase Player
-    bg_scroll += scroll                                                   #Esta variable va sumando de manera continua el progreso del scroll
+    while running:
 
-    if bg_scroll >= 900:                                                  #Si se pasa la resolucion del primer fondo, reiniciar a 0 para volver a verlo al principio
-      bg_scroll =0
-    draw_bg(bg_scroll)                                                    #Imprimir fondo
-    
-    player.draw()                                                         #Imprimir sprites
-    
-    for a in asteroids:
-      a.draw(screen)
-      a.x += a.xv
-      a.y += a.yv
+        clock.tick(FPS)  # Setea los FPS a 60
+        asteroidCount += 1
+        scroll = player.move()  # Agrega funcionalidad de movimiento en la clase Player
+        bg_scroll += (
+            scroll  # Esta variable va sumando de manera continua el progreso del scroll
+        )
 
-      if (player.rect.left >= a.x and player.rect.left <= a.x + a.w) or (player.rect.right + player.width >= a.x and player.rect.right + player.width <= a.x + a.w):
-        if (player.rect.top >= a.y and player.rect.top <= a.y + a.h) or (player.rect.bottom + player.height >= a.y and player.rect.bottom + player.height <= a.y + a.h):
-          pygame.quit()      
-    
-    pygame.draw.line(screen, BLANCO, (0, 200), (600, 200))                #Linea que indica cuando debe empezar a mover la camara (Scroll) TEST
+        if (
+            bg_scroll >= 900
+        ):  # Si se pasa la resolucion del primer fondo, reiniciar a 0 para volver a verlo al principio
+            bg_scroll = 0
+        draw_bg(bg_scroll)  # Imprimir fondo
 
-    if asteroidCount % 50 == 0:
-      ran = random.choice([1,1,1,2,2,3])
-      asteroids.append(Asteroid(ran))
+        player.draw()  # Imprimir sprites
 
-    # capturador de eventos
-    for event in pygame.event.get():
-      # detección de salida de ventana
-      if event.type == pygame.QUIT:
-        running = False
-        pygame.quit()
-        sys.exit()
+        for a in asteroids:
+            a.draw(screen)
+            a.x += a.xv
+            a.y += a.yv
 
-    score += 1
-    scoreDisplay = font.render("Puntuación: " + str(score), True, (255,255,255))
-    screen.blit(scoreDisplay,(10,10))
+            if (player.rect.left >= a.x and player.rect.left <= a.x + a.w) or (
+                player.rect.right + player.width >= a.x
+                and player.rect.right + player.width <= a.x + a.w
+            ):
+                if (player.rect.top >= a.y and player.rect.top <= a.y + a.h) or (
+                    player.rect.bottom + player.height >= a.y
+                    and player.rect.bottom + player.height <= a.y + a.h
+                ):
+                    pygame.quit()
 
-    pygame.display.update()
+        pygame.draw.line(
+            screen, BLANCO, (0, 200), (600, 200)
+        )  # Linea que indica cuando debe empezar a mover la camara (Scroll) TEST
+
+        if asteroidCount % 50 == 0:
+            ran = random.choice([1, 1, 1, 2, 2, 3])
+            asteroids.append(Asteroid(ran))
+
+        # capturador de eventos
+        for event in pygame.event.get():
+            # detección de salida de ventana
+            if event.type == pygame.QUIT:
+                running = False
+                pygame.quit()
+                sys.exit()
+
+        score += 1
+        scoreDisplay = font.render("Puntuación: " + str(score), True, (255, 255, 255))
+        screen.blit(scoreDisplay, (10, 10))
+
+        pygame.display.update()
