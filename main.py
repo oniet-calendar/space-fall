f08f9b620ef438ca8f13afa149ab17c07c9d31be
Federico Torres <federicofederico881@gmail.com>
2022-10-15 22:09:51 -0300

    feat: cambiar main menu

--- main.py
+++ main.py
@@ -16,17 +16,19 @@ def getFont(fontSize):
 #cargar imagenes 
 bg_image = pygame.image.load("img/background.png").convert_alpha()
 pygame.display.set_icon(pygame.image.load("img/logo.png").convert_alpha())
+player_sprite = pygame.image.load("img/Astronaut_Falling.png").convert_alpha()
 
 # Menu principal
 def main_menu(): 
     # titulo de la ventana 
     pygame.display.set_caption("Space Fall")
-    while True:  
+    while True:
+        screen.blit(bg_image, (0,  0))
         screen.blit(bg_image, (0,  0))
         MENU_MOUSE_POS = pygame.mouse.get_pos()
  
-        MENU_TEXT = getFont(100).render("Space Fall", True, "#ffffff")
-        MENU_RECT = MENU_TEXT.get_rect(center=(300, 100))
+        MENU_TEXT = getFont(75).render("Space Fall", True, "#ffffff")
+        MENU_RECT = MENU_TEXT.get_rect(center=(250, 140))
  
         PLAY_BUTTON = Button(image=pygame.image.load("img/play.png"), pos=(300, 350), 
                             text_input="Jugar", font=getFont(50), base_color="#d7fcd4", hovering_color="White")
@@ -35,6 +37,7 @@ def main_menu():
         QUIT_BUTTON = Button(image=pygame.image.load("img/quit.png"), pos=(300, 610), 
                             text_input="Salir del juego", font=getFont(30), base_color="#d7fcd4", hovering_color="White")
         screen.blit(MENU_TEXT, MENU_RECT)
+        screen.blit(pygame.transform.flip(pygame.transform.scale(player_sprite, (120,120)),True,False),(430,60))
         for button in [PLAY_BUTTON, TUTORIAL_BUTTON, QUIT_BUTTON]:
             button.changeColor(MENU_MOUSE_POS)
             button.update(screen)
