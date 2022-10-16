34d3602a641cf7d1a8dfcc6801a1122a8d55c69f
Federico Torres <federicofederico881@gmail.com>
2022-10-15 22:51:02 -0300

    feat: añadir loop de musica infinito y fade

--- music.py
+++ music.py
@@ -3,5 +3,5 @@ from pygame import mixer
 def background_music():
     mixer.init()
     mixer.music.load('./sounds/space-fall-theme.mp3')
-    mixer.music.play()
+    mixer.music.play(loops=25,fade_ms=700)
     mixer.music.set_volume(0.7)
