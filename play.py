07598828a417601ad1388485b626259c72319daf
JuanSaz <juan.snchez03@gmail.com>
2022-10-17 22:17:09 -0300

    refactor: cambiar variables para mejorar el gameplay

--- play.py
+++ play.py
@@ -57,6 +57,91 @@ class Asteroid(object):
       self.rect.x = self.xv + self.x
       self.rect.y += self.yv
 
+class Player2:  # Clase del jugador
+    def __init__(self, x, y):
+        self.image = pygame.transform.scale(
+            player_sprite, (80, 80)
+        )  # Inicializa y reescala el sprite
+        self.height = 55
+        self.width = 40
+        self.rect = pygame.Rect(
+            0, 0, self.width, self.height
+        )  # Agrega un rectangulo para colisiones
+        self.rect.center = (
+            x,
+            y,
+        )  # Posiciona este rectangulo en las coordenadas iniciales
+        self.vel_y = 0
+        self.flip = False  # Inicia con la imagen flipped False
+        self.cool_down_count = 0
+        #jetpack_amount = 10
+
+    def cool_down(self):
+        if self.cool_down_count >= 3:                                  #Si se quiere modificar el tiempo de prevencion de spam, variar este y
+            self.cool_down_count == 0
+        elif self.cool_down_count > 0:
+            self.cool_down_count += 1
+
+    def move(self):  # Dependiendo de que tecla se toca:
+        scroll = 0
+        dx = 0
+        dy = 0
+        self.cool_down()
+
+        key = pygame.key.get_pressed()
+        if key[pygame.K_j]:
+            dx -= 12
+            self.flip = True
+        if key[pygame.K_l]:
+            dx += 12
+            self.flip = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    if self.cool_down_count == 0:
+                        self.vel_y = -10
+                        self.cool_down_count = 1
+
+                    if self.cool_down_count == 3:                                              #Si se quiere modificar el tiempo de prevencion de spam, variar este y
+                        self.vel_y = -10
+                        self.cool_down_count = 1
+
+        # Seteo gravedad
+        self.vel_y += GRAVITY
+        dy += self.vel_y
+
+        # delimitar el movimiento para evitar cruzar los margenes verticales // Pantalla de Perdiste
+        if self.rect.bottom + dy > 900:
+            pygame.quit()
+            sys.exit()
+            #dy = 0      #TEST: para no caer en el vacio, se frena la velocidad de caída
+
+        # delimitar el movimiento para evitar cruzar los margenes laterales
+        if self.rect.left + dx < 0:
+            dx = -self.rect.left
+        if self.rect.right + dx > 600:
+            dx = 600 - self.rect.right
+
+        if self.rect.top <= 200:
+            if self.vel_y < 0:
+                scroll = -dy
+
+        # actualizo el valor de movimiento por variables almacenando movimiento en pixeles
+        self.rect.x += dx
+        self.rect.y += dy + scroll
+
+        return scroll
+
+    def draw(
+        self,
+    ):  # Funcion dedicada a imprimir el sprite // Dependiendo su direccion, se flipea el sprite
+        screen.blit(
+            pygame.transform.flip(self.image, self.flip, False),
+            (self.rect.x - 20, self.rect.y - 5),
+        )
+        #pygame.draw.rect(screen, BLANCO, self.rect, 2)
+
 
 class Player:  # Clase del jugador
     def __init__(self, x, y):
@@ -78,7 +163,7 @@ class Player:  # Clase del jugador
         #jetpack_amount = 10
 
     def cool_down(self):
-        if self.cool_down_count >= 10:                                  #Si se quiere modificar el tiempo de prevencion de spam, variar este y
+        if self.cool_down_count >= 3:                                  #Si se quiere modificar el tiempo de prevencion de spam, variar este y
             self.cool_down_count == 0
         elif self.cool_down_count > 0:
             self.cool_down_count += 1
@@ -101,11 +186,11 @@ class Player:  # Clase del jugador
             if event.type == pygame.MOUSEBUTTONDOWN:
                 if event.button == 1:
                     if self.cool_down_count == 0:
-                        self.vel_y = -20
+                        self.vel_y = -15
                         self.cool_down_count = 1
 
-                    if self.cool_down_count == 10:                                              #Si se quiere modificar el tiempo de prevencion de spam, variar este y
-                        self.vel_y = -20
+                    if self.cool_down_count == 3:                                              #Si se quiere modificar el tiempo de prevencion de spam, variar este y
+                        self.vel_y = -15
                         self.cool_down_count = 1
 
         # Seteo gravedad
